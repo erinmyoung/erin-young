8ac43522f273daf62ad49553bd51c016a7793ac9
Erin Young <erinmyoung12@gmail.com>
2022-07-24 11:01:51 +0100

    Remove image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,6 @@
 <template>
   <main>
-    <img
-      alt="Crater Lake painting"
-      class="image"
-      src="@/assets/art-craterlake.png"
-      width="200"
-      height="300"
-    />
+
   </main>
 </template>
 
